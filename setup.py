7230c0ed3816fb37c2b73b8a5fa68a245af0873c
Chris Mulligan <chris.mulligan@gmail.com>
2010-05-24 19:37:50 -0400

    Added additional metadata to setup.py.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(name="pmxbot",
     version="1001",
-    author="You Gov, Plc. (jamwt, mrshoe, cperry, chmullig, and others)",
-    author_email="open.source@yougov.com",
     packages=["pmxbot"],
     package_data={'pmxbot' : ["popquotes.sqlite"]},
     entry_points={
@@ -18,6 +16,39 @@ pmxbot=pmxbot.pmxbot:run
         "simplejson",
         "httplib2",
         "feedparser",
-    ]
+    ],
+    description="IRC bot - full featured, yet extensible and customizable",
+    license = 'MIT',
+    author="You Gov, Plc. (jamwt, mrshoe, cperry, chmullig, and others)",
+    author_email="open.source@yougov.com",
+    maintainer = 'chmullig',
+    maintainer_email = 'chmullig@gmail.com',
+    url = 'http://bitbucket.org/yougov/pmxbot',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+    ],
+    long_description = """
+    pmxbot
+    ======
+    
+    pmxbot is an IRC bot written in python. Originally built for internal use,
+    it's been sanitized and set free upon the world.
+    
+    
+    Commands
+    ========
+    pmxbot listens to commands prefixed by a '!'
+    If it's a command it knows it will reply, take an action, etc.
+    It can search the web, quote you, track karma, make decisions,
+    and do just about anything else you could want. It logs text in a sqlite3
+    database, and eventually we'll write a web interface to it.
+    """,
     )
     
