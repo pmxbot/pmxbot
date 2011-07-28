f83d4e4829e3c3b073d61e536d6ade909833d4e3
Jason R. Coombs <jaraco@jaraco.com>
2011-07-28 10:21:15 -0400

    Added a test to ensure that that popquotes commands are registered properly

--- tests/unit/test_popquotes.py
+++ tests/unit/test_popquotes.py
@@ -1,6 +1,7 @@
 import re
 
 from pmxbot import popquotes
+from pmxbot import botbase
 
 quote_pattern = re.compile('\(\d+/\d+\): .+')
 
@@ -8,3 +9,8 @@ def test_bender():
 	res = popquotes.bartletts('bender', 'somenick', '')
 	assert res is not None
 	assert quote_pattern.match(res)
+
+def test_registered():
+	handlers = botbase._handler_registry
+	all_names = [handler[1] for handler in handlers]
+	assert 'bender' in all_names
