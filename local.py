ad1131a3382963a4a17f78ed12194aeb67a2a240
Jason R. Coombs <jaraco@jaraco.com>
2012-08-24 15:14:12 -0400

    Clean up lint and long lines in local.py

--- local.py
+++ local.py
@@ -2,7 +2,14 @@
 Local pmxbot extensions, incude the path to this file in your config.yaml.
 """
 
-@command("yahoolunch", doc="Find a random neary restaurant for lunch using Yahoo Local. Defaults to 1 mile radius, but append Xmi to the end to change the radius.")
+import random
+import urllib
+
+from pmxbot import command, execdelay
+
+@command("yahoolunch", doc="Find a random neary restaurant for lunch using "
+    "Yahoo Local. Defaults to 1 mile radius, but append Xmi to the end to "
+    "change the radius.")
 def lunch(client, event, channel, nick, rest):
     yahooid = "eeGETYOUROWN.yu"
     from yahoo.search.local import LocalSearch
@@ -14,27 +21,31 @@ def lunch(client, event, channel, nick, rest):
         radius = float(radius.replace('mi', ''))
     else:
         radius = 1
-    srch = LocalSearch(app_id=yahooid, category=96926236, results=20, query="lunch", location=location, radius=radius)
+    srch = LocalSearch(app_id=yahooid, category=96926236, results=20,
+        query="lunch", location=location, radius=radius)
     res = srch.parse_results()
-    max = res.totalResultsAvailable if res.totalResultsAvailable < 250 else 250
-    num = random.randint(1, max) - 1
+    limit = min(250, res.totalResultsAvailable)
+    num = random.randint(1, limit) - 1
     if num < 19:
-        choice = res.results[num]
+        selection = res.results[num]
     else:
-        srch = LocalSearch(app_id=yahooid, category=96926236, results=20, query="lunch", location=location, start=num)
+        srch = LocalSearch(app_id=yahooid, category=96926236, results=20,
+            query="lunch", location=location, start=num)
         res = srch.parse_results()
-        choice = res.results[0]
-    return '%s @ %s - %s' % (choice['Title'], choice['Address'], choice['Url'])
-    
-@command("paste", aliases=(), doc="Drop a link to your latest paste on http://libpa.st")
+        selection = res.results[0]
+    return '{Title} @ {Address} - {Url}'.format(**selection)
+
+@command("paste", aliases=(), doc="Drop a link to your latest paste on "
+    "http://libpa.st")
 def paste(client, event, channel, nick, rest):
     request = urllib.urlopen("http://libpa.st/last/%s" % nick)
     post_url = request.geturl()
     if post_url and request.getcode() == 200:
         return post_url
     else:
-        return "hmm.. I didn't find a recent paste of yours, %s. Checkout http://libpa.st" % nick
+        return ("hmm.. I didn't find a recent paste of yours, %s. Checkout "
+            "http://libpa.st" % nick)
 
 @execdelay("hi", channel="#botone", howlong=10)
 def howdy(client, event):
-	return "Howdy everybody!"
+    return "Howdy everybody!"
