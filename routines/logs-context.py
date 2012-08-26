9674c1066977164ed831c2e4169479354a323665
Jason R. Coombs <jaraco@jaraco.com>
2012-08-25 22:52:05 -0400

    Logs context no longer needed

--- routines/logs-context.py
+++
@@ -1,7 +0,0 @@
-from pmxbot import logging
-mlog = logging.Logger.from_URI('mongodb://localhost')
-slog = logging.Logger.from_URI('sqlite:pmxbot.sqlite')
-for msg_pair in zip(mlog.all_messages(), slog.export_all()):
-	mmsg, smsg = msg_pair
-	if mmsg['message'] != smsg['message']: break
-
