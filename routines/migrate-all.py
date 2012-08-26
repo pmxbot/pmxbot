affa530a77a4a5dc44a6a46887626a51d7a89551
Jason R. Coombs <jaraco@jaraco.com>
2012-08-25 22:56:02 -0400

    Update migration script so it only runs if executed as a script. Also updated module references.

--- routines/migrate-all.py
+++ routines/migrate-all.py
@@ -1,5 +1,13 @@
-from pmxbot import logging
-from pmxbot import util
-from pmxbot import rss
-from pmxbot import storage
-storage.migrate_all('sqlite:pmxbot.sqlite', 'mongodb://localhost')
+import importlib
+
+import pmxbot.storage
+
+def run():
+	# load the storage classes so the migration routine will find them.
+	for mod in ('pmxbot.logging', 'pmxbot.karma', 'pmxbot.quotes',
+			'pmxbot.rss'):
+		importlib.import_module(mod)
+	pmxbot.storage.migrate_all('sqlite:pmxbot.sqlite', 'mongodb://localhost')
+
+if __name__ == '__main__':
+	run()
