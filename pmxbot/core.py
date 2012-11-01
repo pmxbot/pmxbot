6ff155edd7f71220bc61f3762cfecc54395b434a
Jason R. Coombs <jaraco@jaraco.com>
2012-11-01 18:20:51 -0400

    'log' was already in locals(). Blarg.

--- pmxbot/core.py
+++ pmxbot/core.py
@@ -83,7 +83,7 @@ class LoggingCommandBot(irc.bot.SingleServerIRCBot):
 			s = s.split(' ', 1)[-1].lstrip()
 			log = False
 		if len(s) > 510:
-			log.warn(u"Truncating long message: " + s)
+			globals()['log'].warn(u"Truncating long message: " + s)
 			s = s[:510]
 		func(channel, s)
 		if (
