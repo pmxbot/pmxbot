eb47a40085e3699f1ba1deda21e517bb61b112e3
Jason R. Coombs <jaraco@jaraco.com>
2013-01-02 15:57:33 -0500

    Added test proving rate limiting is also working (to some extent)

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -6,3 +6,14 @@ def test_contains_always_match():
 	"""
 	handler = core.ContainsHandler(name='#', func=None)
 	assert handler.match('Tell me about #foo', channel='bar')
+
+def test_contains_rate_limit():
+	"""
+	Contains handler with a rate should only appear sometimes.
+	"""
+	handler = core.ContainsHandler(name='#', func=None, rate=0.5)
+	results = set(
+		handler.match('Tell me about #foo', channel='bar')
+		for x in xrange(1000)
+	)
+	assert True in results and False in results
