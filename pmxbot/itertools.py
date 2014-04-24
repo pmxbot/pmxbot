9020acb36a509d5823df71e712430a69e94e997a
Jason R. Coombs <jaraco@jaraco.com>
2014-04-23 22:13:13 -0400

    Use always_iterable from jaraco.util

--- pmxbot/itertools.py
+++ pmxbot/itertools.py
@@ -1,42 +1,4 @@
-import io
-
-import six
-
-def always_iterable(item):
-	r"""
-	Given an item from a pmxbot handler, always return an iterable.
-
-	If the item is None, return an empty iterable.
-	>>> list(always_iterable(None))
-	[]
-
-	If the item is a string, return an iterable of the lines in the string.
-	>>> print('/'.join(always_iterable('foo')))
-	foo
-	>>> print('/'.join(always_iterable('foo\nbar')))
-	foo/bar
-
-	>>> list(always_iterable([1,2,3]))
-	[1, 2, 3]
-
-	>>> always_iterable(iter('abc'))  # doctest: +ELLIPSIS
-	<...iterator ...>
-
-	And any other non-iterable objects are returned as single-tuples of that
-	item.
-	>>> list(always_iterable(object()))  # doctest: +ELLIPSIS
-	[<object object at ...>]
-	"""
-	if item is None:
-		item = ()
-
-	if isinstance(item, six.string_types):
-		item = (line.rstrip('\n') for line in io.StringIO(six.text_type(item)))
-
-	if not hasattr(item, '__iter__'):
-		item = item,
-
-	return item
+from jaraco.util.itertools import always_iterable
 
 def generate_results(function):
 	"""
