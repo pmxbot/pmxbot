213a9b3f90b452ec5260bbade2dde38040b16833
Jason R. Coombs <jaraco@jaraco.com>
2014-08-08 14:43:11 -0400

    Don't run routines during doctests

--- routines/dedup-karma.py
+++ routines/dedup-karma.py
@@ -2,9 +2,13 @@ import sys
 import urllib.parse
 import pymongo
 
-url = sys.argv[1]
-db_name = urllib.parse.urlparse(url).path.lstrip('/')
-db = pymongo.MongoClient(url)[db_name]
-for entry in db.karma.find():
-	entry['names'] = list(set(entry['names']))
-	db.karma.save(entry)
+def run():
+	url = sys.argv[1]
+	db_name = urllib.parse.urlparse(url).path.lstrip('/')
+	db = pymongo.MongoClient(url)[db_name]
+	for entry in db.karma.find():
+		entry['names'] = list(set(entry['names']))
+		db.karma.save(entry)
+
+if __name__ == '__main__':
+	run()
--- routines/rebuild-recent.py
+++ routines/rebuild-recent.py
@@ -2,10 +2,14 @@ import sys
 import urllib.parse
 import pymongo
 
-url = sys.argv[1]
-db_name = urllib.parse.urlparse(url).path.lstrip('/')
-db = pymongo.MongoClient(url)[db_name]
-for entry in db.logs.find():
-	entry['ref'] = entry.pop('_id')
-	spec = dict(channel=entry['channel'])
-	db.recent.update(spec, entry, upsert=True)
+def run():
+	url = sys.argv[1]
+	db_name = urllib.parse.urlparse(url).path.lstrip('/')
+	db = pymongo.MongoClient(url)[db_name]
+	for entry in db.logs.find():
+		entry['ref'] = entry.pop('_id')
+		spec = dict(channel=entry['channel'])
+		db.recent.update(spec, entry, upsert=True)
+
+if __name__ == '__main__':
+	run()
