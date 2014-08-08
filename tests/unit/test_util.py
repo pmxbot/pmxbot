d52c9731b0c6494e9f4181fc33f00cdf39adb3ca
Jason R. Coombs <jaraco@jaraco.com>
2014-08-08 08:52:55 -0400

    Add test for emergency compliments

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -5,3 +5,7 @@ from pmxbot import util
 @pytest.has_wordnik
 def test_lookup():
 	assert util.lookup('dachshund') is not None
+
+@pytest.has_internet
+def test_emergency_compliment():
+	assert util.load_emergency_compliments()
