070e44686d28a1ce70ed5cf4810546dc09795bd7
Jason R. Coombs <jaraco@jaraco.com>
2016-04-16 19:54:56 +0100

    Move paste command to librarypaste library.

--- pmxbot/commands.py
+++ pmxbot/commands.py
@@ -616,28 +616,6 @@ def blame(client, event, channel, nick, rest):
 		yield "/me cries and weeps in despair"
 
 
-def _request_friendly(auth):
-	"""
-	Requests does strict type checking on the auth. If it's not a tuple, it
-	tries to call it.
-	"""
-	if auth is not None:
-		return tuple(auth)
-
-
-@command()
-def paste(client, event, channel, nick, rest):
-	"Drop a link to your latest paste"
-	path = '/last/{nick}'.format(**vars())
-	paste_root = pmxbot.config.get('librarypaste', 'http://paste.jaraco.com')
-	url = urllib.parse.urljoin(paste_root, path)
-	auth = pmxbot.config.get('librarypaste auth')
-	resp = requests.head(url, auth=_request_friendly(auth))
-	if not resp.ok:
-		return "I couldn't resolve a recent paste of yours. Maybe try " + url
-	return resp.headers['location']
-
-
 @contains('pmxbot', channels='unlogged', rate=.3)
 def rand_bot(client, event, channel, nick, rest):
 	normal_functions = [
