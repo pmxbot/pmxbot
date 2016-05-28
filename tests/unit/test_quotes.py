c76a807d95311cc02ce87ff22b339489e7b42635
Jason R. Coombs <jaraco@jaraco.com>
2016-05-27 22:35:57 -0400

    Extract clean quotes as a fixture.

--- tests/unit/test_quotes.py
+++ tests/unit/test_quotes.py
@@ -1,20 +1,29 @@
+import pytest
+
 from pmxbot import quotes
 
 
-def test_MongoDBQuotes(mongodb_uri):
+@pytest.yield_fixture
+def clean_quotes(mongodb_uri):
 	q = quotes.Quotes.from_URI(mongodb_uri)
 	q.lib = 'test'
 	clean = lambda: q.db.remove({'library': 'test'})
 	clean()
 	try:
-		q.add('who would ever say such a thing')
-		q.add('go ahead, take my pay')
-		q.add("let's do the Time Warp again")
-		qt, i, n = q.lookup('time warp')
-		assert qt.startswith("let's")
-		q.lookup('nonexistent')
-		q.delete('Time Warp')
-		qt, i, n = q.lookup('Time Warp')
-		assert qt == ''
+		yield q
 	finally:
 		clean()
+
+
+def test_MongoDBQuotes(clean_quotes):
+	q = clean_quotes
+
+	q.add('who would ever say such a thing')
+	q.add('go ahead, take my pay')
+	q.add("let's do the Time Warp again")
+	qt, i, n = q.lookup('time warp')
+	assert qt.startswith("let's")
+	q.lookup('nonexistent')
+	q.delete('Time Warp')
+	qt, i, n = q.lookup('Time Warp')
+	assert qt == ''
