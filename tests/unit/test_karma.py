b8d8def419d4bd7b55832821488c087e6d3d2bbb
Jason R. Coombs <jaraco@jaraco.com>
2017-03-06 15:02:10 -0500

    Rewrite test using pytest fixture.

--- tests/unit/test_karma.py
+++ tests/unit/test_karma.py
@@ -8,20 +8,17 @@ from pmxbot import karma
 
 
 class TestMongoDBKarma:
-	def setup_karma(self, mongodb_uri):
+	@pytest.fixture
+	def mongodb_karma(self, request, mongodb_uri):
 		k = karma.Karma.from_URI(mongodb_uri)
 		k.db = k.db.database.connection[
 			k.db.database.name + '_test'
 		][k.db.name]
-		self.karma = k
+		request.addfinalizer(k.db.drop)
+		return k
 
-	def teardown_method(self, method):
-		if hasattr(self, 'karma'):
-			self.karma.db.drop()
-
-	def test_basic_usage(self, mongodb_uri):
-		self.setup_karma(mongodb_uri)
-		k = self.karma
+	def test_basic_usage(self, mongodb_karma):
+		k = mongodb_karma
 		k.change('foo', 1)
 		k.change('bar', 1)
 		k.set('baz', 3)
@@ -33,17 +30,15 @@ class TestMongoDBKarma:
 		k.change('foo', 1)
 		assert k.lookup('foo') == k.lookup('bar') == 5
 
-	def test_linking_same_does_nothing(self, mongodb_uri):
-		self.setup_karma(mongodb_uri)
-		k = self.karma
+	def test_linking_same_does_nothing(self, mongodb_karma):
+		k = mongodb_karma
 		k.set('foo', 99)
 		with pytest.raises(karma.SameName):
 			k.link('foo', 'foo')
 		assert k.lookup('foo') == 99
 
-	def test_linking_similar(self, mongodb_uri):
-		self.setup_karma(mongodb_uri)
-		k = self.karma
+	def test_linking_similar(self, mongodb_karma):
+		k = mongodb_karma
 		k.set('foo', 99)
 		k.set('foo|away', 1)
 		k.link('foo', 'foo|away')
@@ -53,9 +48,8 @@ class TestMongoDBKarma:
 		with pytest.raises(karma.AlreadyLinked):
 			k.link('foo|away', 'foo')
 
-	def test_already_linked_raises_error(self, mongodb_uri):
-		self.setup_karma(mongodb_uri)
-		k = self.karma
+	def test_already_linked_raises_error(self, mongodb_karma):
+		k = mongodb_karma
 		k.set('foo', 50)
 		k.set('bar', 50)
 		k.link('foo', 'bar')
