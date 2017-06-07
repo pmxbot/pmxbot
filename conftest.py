767a1c6d9567404f99da68c8860acafea4edf76c
Jason R. Coombs <jaraco@jaraco.com>
2017-06-07 10:00:39 -0400

    Ensure that pmxbot.config is defined when running tests.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,11 @@
+import pytest
 
 
 # This test must be in the root directory.
 def pytest_addoption(parser):
 	parser.addoption("--runslow", action="store_true", help="run slow tests")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def init_config():
+	__import__('pmxbot').config = {}
