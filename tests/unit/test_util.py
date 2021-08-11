ea274f24cba9c3d4963cceab2bfe0ab0719810d7
Jason R. Coombs <jaraco@jaraco.com>
2021-08-10 20:00:45 -0400

    xfail test_emergency_compliment. Workaround for #97.

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -8,6 +8,7 @@ def test_lookup(needs_wordnik):
     assert util.lookup('dachshund') is not None
 
 
+@pytest.mark.xfail(reason="#97: Google is unreliable")
 def test_emergency_compliment(needs_internet):
     assert util.load_emergency_compliments()
 
