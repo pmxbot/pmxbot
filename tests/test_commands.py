3adc54cb404900ec5fe30dc6a32a3c9e73171257
Jason R. Coombs <jaraco@jaraco.com>
2023-06-24 23:11:25 -0400

    Remove 'setup_class', not relevant to the failure.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,9 +4,5 @@ from pmxbot import logging
 
 
 class TestCommands:
-    @classmethod
-    def setup_class(cls):
-        assert importlib.import_module('pmxbot.logging') is logging
-
     def test_boo(self):
-        ...
+        assert importlib.import_module('pmxbot.logging') is logging
