f5c83ff0db344995822229de6000d6d9c3e0886f
Jason R. Coombs <jaraco@jaraco.com>
2023-06-24 22:07:45 -0400

    Remove all but test_commands / test_boo

--- tests/unit/test_viewer.py
+++
@@ -1,9 +0,0 @@
-import pmxbot.web.viewer
-
-
-def test_get_context():
-    """
-    Calling get_context should return a dictionary.
-    """
-    pmxbot.web.viewer.init_config()
-    assert isinstance(pmxbot.web.viewer.HelpPage.get_context(), dict)
