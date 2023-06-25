514cd93afcdfba33aac02b920f427389769f79c3
Jason R. Coombs <jaraco@jaraco.com>
2023-06-24 22:53:30 -0400

    Clean up test

--- tests/unit/test_commands.py
+++ tests/unit/test_commands.py
@@ -3,15 +3,6 @@ import importlib
 from pmxbot import logging
 
 
-def logical_xor(a, b):
-    return bool(a) ^ bool(b)
-
-
-def onetrue(*args):
-    truthiness = list(filter(bool, args))
-    return len(truthiness) == 1
-
-
 class TestCommands:
     @classmethod
     def setup_class(cls):
